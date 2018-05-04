8ef567a89410b4d7d7c40cc457d23e250660948a
Sen <sen@sens-mbp-2.ait-sydney.net.au>
2018-05-04 16:56:20 +1000

    first ionic commit

---
+++ src/pages/list/list.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ActionSheetController } from 'ionic-angular';
+
+/**
+ * Generated class for the ListPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-list',
+  templateUrl: 'list.html',
+})
+export class ListPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,private actionSheetCtrl: ActionSheetController) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ListPage');
+  }
+  presentActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Modify your album',
+      buttons: [
+        {
+          text: 'Destructive',
+          role: 'destructive',
+          handler: () => {
+            console.log('Destructive clicked');
+          }
+        },{
+          text: 'Archive',
+          handler: () => {
+            console.log('Archive clicked');
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+}
